8e9f5e5761ab17086779bebe07666e07a03e0d9e
slittleton <littletonltp@gmail.com>
2019-08-22 13:09:27 -0700

    userinfo/front

--- ecomm1-client/src/components/account/AddressForm.js
+++ ecomm1-client/src/components/account/AddressForm.js
@@ -1,6 +1,61 @@
-import React from 'react';
+import React from "react";
 
-const AddressForm = () =>{
-  return <div>ADDRESS FORM</div>
-}
+const AddressForm = ({ handleChange, address }) => {
+  return (
+    <div className="create-form box">
+      {/* {JSON.stringify(address)} */}
+      <div className="subtitle center top-margin">Address</div>
+      <div className="form-control-create">
+        <div className="center small-pad">Name:</div>
+        <input
+          type="text"
+          className="form-field-create"
+          name="name"
+          value={address.name}
+          onChange={handleChange("name")}
+        />
+      </div>
+      <div className="form-control-create">
+        <div className="center small-pad">Street:</div>
+        <input
+          type="text"
+          className="form-field-create"
+          name="street"
+          value={address.street}
+          onChange={handleChange("street")}
+        />
+      </div>
+      <div className="form-control-create">
+        <div className="center small-pad">City:</div>
+        <input
+          type="text"
+          className="form-field-create"
+          name="city"
+          value={address.city}
+          onChange={handleChange("city")}
+        />
+      </div>
+      <div className="form-control-create">
+        <div className="center small-pad">State:</div>
+        <input
+          type="text"
+          className="form-field-create"
+          name="state"
+          value={address.state}
+          onChange={handleChange("state")}
+        />
+      </div>
+      <div className="form-control-create">
+        <div className="center small-pad">Zipcode:</div>
+        <input
+          type="text"
+          className="form-field-create"
+          name="zipcode"
+          value={address.zipcode}
+          onChange={handleChange("zipcode")}
+        />
+      </div>
+    </div>
+  );
+};
 export default AddressForm;
--- ecomm1-client/src/components/account/AdminSettings.js
+++ ecomm1-client/src/components/account/AdminSettings.js
@@ -1,33 +1,104 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../layout/Layout";
 import { connect } from "react-redux";
 import AdminMenu from "../layout/AdminMenu";
+import AddressForm from "./AddressForm";
 
-const AdminSettings = (props) => {
+const AdminSettings = props => {
+  const [values, setValues] = useState({
+    userName: "",
+    email: "",
+    address: { name: "", street: "", city: "", state: "", zipcode: "" }
+  });
+  const { userName, email, address } = values;
+
+  useEffect(() => {
+    const { userName, userEmail, userAddress } = props.user;
+    setValues({
+      ...values,
+      name: userName,
+      email: userEmail,
+    });
+    if(userAddress){ setValues({...values, address: userAddress})}
+  }, []);
+
+  const handleSubmit = () => {};
+
+  const handleChange = name => async e  => {
+    await setValues({ ...values, [name]: e.target.value });
+  };
+
   return (
     <div className="">
       <Layout
         title="Admin Account - Settings"
-        description={`Welcome ${props.user.userName? props.user.userName: null}`}
+        description={`Welcome ${
+          props.user.userName ? props.user.userName : null
+        }`}
         accountMenu={<AdminMenu />}
       >
-        <div className="settings">
-          <h3 className="admin-title">Settings</h3>
-          <div className="settings-box">
-            
+        <div className="settings container box">
+          <div className="box small-pad">
+            <div className="title">Current User Information</div>
+            <div className="tiny-pad">Name: {props.user.userName}</div>
+            <div className="tiny-pad">Email: {props.user.userEmail}</div>
+            <div className="tiny-pad">
+              Address:{" "}
+              {props.user.userAddress
+                ? props.user.userAddress
+                : "No Address On File"}
+            </div>
+          </div>
+
+          <div className="">
+            <div className="title">Update User Information</div>
+            <form onSubmit={handleSubmit} className="create-form">
+              <div className="box">
+                <div className="subtitle center top-margin">
+                  Username and Email
+                </div>
+                <div className="form-control-create">
+                  <div className="center small-pad">Username:</div>
+                  <input
+                    type="text"
+                    className="form-field-create"
+                    name="name"
+                    value={userName}
+                    onChange={handleChange("name")}
+                  />
+                </div>
+                <div className="form-control-create">
+                  <div className="center small-pad">Email:</div>
+                  <input
+                    type="text"
+                    className="form-field-create"
+                    name="email"
+                    value={email}
+                    onChange={handleChange("email")}
+                  />
+                </div>
+              </div>
+              <AddressForm handleChange={handleChange} address={address} />
+
+              <button className="btn btn-margin" type="submit">
+                Submit Update Info
+              </button>
+            </form>
           </div>
         </div>
-
       </Layout>
     </div>
   );
-
-}
+};
 const mapStateToProps = state => {
+  console.log("ADMIN SETTINGS", state);
   return {
-    user: state.authReducer,
-    messages: state.contactReducer,
-    products: state.productsReducer,
-  }
-}
-export default connect(mapStateToProps, {})(AdminSettings)
+    user: state.authReducer
+    // actionStatus: state.adminReducer.actionStatus,
+    // error: state.adminReducer.error
+  };
+};
+export default connect(
+  mapStateToProps,
+  {}
+)(AdminSettings);
